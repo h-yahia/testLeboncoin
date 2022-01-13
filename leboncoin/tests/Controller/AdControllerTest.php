83711c307b1a25f61e45b398e780ac77cf19511a
Habib BEN YAHIA <habib.yahia@gmail.com>
2022-01-13 15:38:05 +0100

    Add post ad test

--- leboncoin/tests/Controller/AdControllerTest.php
+++ leboncoin/tests/Controller/AdControllerTest.php
@@ -22,5 +22,67 @@ class AdControllerTest extends WebTestCase
         $this->assertSame(404, $client->getResponse()->getStatusCode());
     }
 
+    public function testSuccessPostAd(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/ad',
+            array(
+                'title' => 'title test',
+                'content' => 'content test',
+                'category' => 'job'
+            )
+        );
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testErrorPostAd(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/ad',
+            array( // Without content, form not valid
+                'title' => 'title test',
+                'category' => 'job'
+            )
+        );
+        $this->assertSame(400, $client->getResponse()->getStatusCode());
+    }
+
+    public function testSuccessPostAutomobileAd(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/ad',
+            array(
+                'title' => 'title test',
+                'content' => 'content test',
+                'brand' => 'brand test',
+                'model' => 'model test',
+                'category' => 'automobile'
+            )
+        );
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testErrorPostAutomobileAd(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/ad',
+            array( // Without content, form not valid
+                'title' => 'title test',
+                'content' => 'content test',
+                'brand' => 'brand test',
+                'category' => 'automobile'
+            )
+        );
+        $this->assertSame(400, $client->getResponse()->getStatusCode());
+    }
+
 
 }
